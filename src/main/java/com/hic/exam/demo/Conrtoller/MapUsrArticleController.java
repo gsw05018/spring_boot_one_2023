01e3d7934f8757eecc37d414da40fa2e7a82ffab
gsw05018 <gsw05018@naver.com>
2023-08-12 19:27:42 +0900

    delete 기능 구현, getDeleteId 함수 도입

--- src/main/java/com/hic/exam/demo/Conrtoller/MapUsrArticleController.java
+++ src/main/java/com/hic/exam/demo/Conrtoller/MapUsrArticleController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.annotation.RequestScope;
 
 import com.hic.exam.demo.Dto.Article;
 import com.hic.exam.demo.Dto.ResulData;
@@ -19,18 +20,21 @@ public class MapUsrArticleController {
 	MapUsrArticleController() {
 		articles = new ArrayList<>();
 		articleLastId = 0;
+		makeTestData();
 	}
 
+	//write
 	@RequestMapping("/mapUsr/article/doWrite")
 	@ResponseBody
 	public ResulData doWrite(String title, String body) {
-		int id = WriteArticle(title, body);
+		int id = writeArticle(title, body);
 		Article article = getArticleId(id);
 		return new ResulData("S-1", id + "번 글이 생성되었습니다", "aritcle", article);
 
 	}
 
-	private int WriteArticle(String title, String body) {
+	//write 함수
+	private int writeArticle(String title, String body) {
 
 		int id = articleLastId + 1;
 		String regDate = Util.getNowDateStr();
@@ -45,6 +49,7 @@ public class MapUsrArticleController {
 
 	}
 
+	//detail
 	@RequestMapping("/mapUsr/article/getArticle")
 	@ResponseBody
 	public ResulData getArticle(int id) {
@@ -58,7 +63,34 @@ public class MapUsrArticleController {
 		return new ResulData("S-1", id + "번 글입니다", "article", article);
 
 	}
+	
+	//delete
+	@RequestMapping("/mapUsr/article/doDelete")
+	@ResponseBody
+	public ResulData doDelete(int id) {
+		boolean deleted = getDeleteId(id);
+		
+		if(deleted == false) {
+			return new ResulData("F-1", id + "번이 존재하지 않습니다", "id", id);
+		}
+		
+		
+		return new ResulData("s-1", id + "번이 삭제되었습니다", "id", id);
+		
+	}
+	
+	//게시물 삭제할 번호 찾는 함수
+	private boolean getDeleteId(int id) {
+		for(Article article : articles) {
+			if(article.getId() == id) {
+				articles.remove(article);
+				return true;
+			}
+		}
+		return false;
+	}
 
+	//게시물 번호 찾기
 	private Article getArticleId(int id) {
 		for (Article article : articles) {
 			if (article.getId() == id) {
@@ -66,5 +98,11 @@ public class MapUsrArticleController {
 			}
 		}
 		return null;
+	}
+	
+	private void makeTestData(){
+		for(int i = 0; i <= 3; i++) {
+			writeArticle("제목1", "내용1");
+		}
 	}
 }
