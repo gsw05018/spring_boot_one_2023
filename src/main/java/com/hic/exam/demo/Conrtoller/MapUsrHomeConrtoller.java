691a70e05ae4c262fe83c8a760bfbe88f90075fc
gsw05018 <gsw05018@naver.com>
2023-08-13 15:29:34 +0900

    jsp연결 기능 첫 구현: css, js, jsp 파일 추가, pom에 jsp 기능추가, yml에 jsp연결 기능 추가

--- src/main/java/com/hic/exam/demo/Conrtoller/MapUsrHomeConrtoller.java
+++ src/main/java/com/hic/exam/demo/Conrtoller/MapUsrHomeConrtoller.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MapUsrHomeConrtoller {
-	
-	//요청이 왔을 때 어떤 컨트롤러가 호출이 되어야 하는지 알려주는 지표
+
+	// 요청이 왔을 때 어떤 컨트롤러가 호출이 되어야 하는지 알려주는 지표
 	@RequestMapping("/mapUsr/home/main")
-	//자바객체를 http 응답본문의 객체로 변환하여 클라이언트로 전송
-	@ResponseBody
+
+
 	public String showMain() {
-		return "안녕";
+		return "mapUsr/home/main";
 	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/mapUsr/home/main.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="/resource/com.css" />
+<script src="/resource/com.js"defer></script>
+<title>스타일</title>
+</head>
+<body>
+<button onclick = "hello();"> 안녕못하는데?</button>
+<h1>안녕하세요 </h1>
+</body>
+</html>
